cbfce12fe808b122baa30751892a0d205e0e58e0
Antoine Cheron <cheron.antoine@gmail.com>
2021-01-08 14:52:59 +0100

    Implements v5.1.0 for Node server and React client

--- servers/node/src/models/Task.js
+++ servers/node/src/models/Task.js
@@ -77,15 +77,15 @@ const Priority = {
 }
 
 const validateBusinessConstraints = (task, title, description, points, status) => {
-  if (title && (title.length <3 || title.length > 40)) {
+  if (title && (title.length < 4 || title.length > 80)) {
+    return false;
+  } else if (description && description.length > 4000) {
+    return false;
+  } else if (tags && tags.length > 10) {
     return false;
   } else if (points && (points < 0 || points > 120)) {
     return false;
   } else if (tags && tags.length > 10) {
-    return false;
-  } else if (priority && !Object.values(Priority).includes(priority)) {
-    return false;
-  } else if (tags && tags.length > 6) {
     return false;
   } else if (priority && !Object.values(Priority).includes(priority)) {
     return false;
