39af039a2994f62d06b4c86ad1612fe423af66af
Antoine Cheron <cheron.antoine@gmail.com>
2021-01-18 19:23:19 +0100

    Fix merge issue

--- servers/node/src/controllers/analytic-controller.js
+++ servers/node/src/controllers/analytic-controller.js
@@ -3,8 +3,9 @@ const express = require('express');
 const Errors = require('../utils/errors');
 const Responses = require('../utils/responses');
 const AuthService = require('../services/auth-service');
+const ReverseRouter = require('../reverse-router');
 
-const analyticController = function(analyticService) {
+const analyticController = function(analyticService, projectService, taskService) {
 
   const router = express.Router();
 
@@ -16,13 +17,29 @@ const analyticController = function(analyticService) {
       if (!maybeAnalytic) {
         Responses.notFound(res)
       } else {
-        Responses.ok(res, maybeAnalytic.representation())
+        const representation = maybeAnalytic.representation(ReverseRouter)
+        representation.resourceId = resolveResourceUri(representation.resourceId, user.id, projectService, taskService)
+        Responses.ok(res, representation)
       }
     }, res);
   }));
 
   return router;
 
+}
+
+function resolveResourceUri(resourceId, userId, projectService, taskService) {
+  try {
+    const project = projectService.findById(resourceId, userId)
+    if (project) return ReverseRouter.forProject(resourceId)
+  } catch (error) {
+    if (error instanceof Errors.NotFound) {
+      const task = taskService.findById(resourceId)
+      if (task) return ReverseRouter.forTask(resourceId, task.projectId)
+    } else {
+      throw error
+    }
+  }
 }
 
 module.exports = analyticController;
